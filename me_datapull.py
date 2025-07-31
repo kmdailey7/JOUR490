4e2f9a5472c6b1bfa29d1bcb6bf425eb997992d5
Katherine Dailey <dailey.km7@gmail.com>
2025-07-31 15:11:29 -0400

    Update me_datapull.py

--- me_datapull.py
+++ me_datapull.py
@@ -1,3 +1,4 @@
 import requests, json
 METEXT=requests.get('https://datacatalog.cookcountyil.gov/resource/cjeq-bs86.json?$limit=999999').text
-data=json.loads(METEXT)
+ME_DATA=json.loads(METEXT)
+ME_DATA[0]
